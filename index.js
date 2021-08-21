fd4cafdb7587de879258be05a6670100539994ad
mishanya644 <ivanov0604041@gmail.com>
2021-08-21 00:24:16 +0000

    started implementing multiuser support using user folders

--- index.js
+++ index.js
@@ -4,14 +4,32 @@ const utils = require('./modules/utils.js');
 const schedule = require('node-schedule');
 const fs = require('fs');
 const xkcd = require('./modules/xkcd.js');
-
+const assert = require('assert')
 
 let bot = new Telegraf(utils.syncFileToStr('./token.txt'));
 let userId = parseInt(utils.syncFileToStr('./user-id.txt'));
 let timezone = 0;
 
+function getUserDirPath(id){
+  let userDirPath = __dirname + '/database/' + id;
+  return userDirPath;
+}
+
+async function initUserDir(id){
+  let userDirPath = getUserDirPath(id);
+  await fs.promises.mkdir(userDirPath);
+  //todo: add more useful user info such as name and stuff...
+  await utils.strToFile('user info!!!', userDirPath + '/user-info.txt');
+}
+
+//DEBUG!!!
+let myId = 1311788757;
+initUserDir(myId);
+
 bot.start(async ctx=>{
-  ctx.reply('Hello!');
+  if (!fs.existsSync(getUserDirPath(ctx.from.id)))
+    await initUserDir(ctx.from.id).catch(console.log);
+  ctx.reply('Hello! type /help to see available commands.');
 });
 
 //xkcd
@@ -20,13 +38,13 @@ const job = schedule.scheduleJob('0 '+ (10 - timezone) +' * * *', async ()=>{
   for (let i = 0; i < 2; i++){
     let xkcdProgress = await utils.getChatProgress('xkcd');
     let xkcdMsg = await xkcd.getMessageFromPost(xkcdProgress);
-    await bot.telegram.sendMessage(userId, xkcdMsg).catch(err=>console.log(err));  
+    await bot.telegram.sendMessage(userId, xkcdMsg).catch(console.log);
     await utils.setChatProgress('xkcd', xkcdProgress + 1);
   }
 });
 
 bot.command('progressInfoDump', async ctx=>{
-  let xkcdProgress = await fs.promises.readFile(__dirname + '/progress-info/xkcd', 'utf8');
+  let xkcdProgress = await fs.promises.readFile(__dirname + '/database/xkcd', 'utf8');
   ctx.reply('xkcd = ' + xkcdProgress);
 });
 
--- modules/utils.js
+++ modules/utils.js
@@ -11,20 +11,24 @@ function syncFileToStr(filePath){
   try{
     return fs.readFileSync(filePath, 'utf8');
   } catch(err) {
-    console.log('Error while reading token.txt');
+    console.log('Error while reading file');
     console.log(err);
     return null;
   }
 }
 
+async function strToFile(str, path){
+  await fs.promises.writeFile(path, str);
+}
+
 async function getChatProgress(source){
-  let progressFilePath = __dirname + '/../progress-info/' + source;
+  let progressFilePath = __dirname + '/../database/' + source;
   let data = await fs.promises.readFile(progressFilePath, 'utf8');
   return parseInt(data);
 }
 
 async function setChatProgress(source, data){
-  fs.promises.writeFile(__dirname + '/../progress-info/' + source, data)
+  fs.promises.writeFile(__dirname + '/../progress/' + source, data)
 }
 
-module.exports = {fetchUrl, syncFileToStr, getChatProgress, setChatProgress};
+module.exports = {fetchUrl, syncFileToStr, getChatProgress, setChatProgress, strToFile};
